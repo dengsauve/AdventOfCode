075dfb2565417c1227b19e8df93436514c82d7a7
dengsauve <dengsauve@yahoo.com>
2018-12-02 21:43:30 -0800

    Finished Day 3 Challenge

---
+++ 2018/c03/aoc_03.rb
@@ -0,0 +1,42 @@
+patterns = File.readlines("input.txt")
+
+fabric = []
+overlaps = 0
+
+1000.times do
+    fabric << []
+end
+
+fabric.each do |spot|
+    1000.times do
+        spot << 0
+    end
+end
+
+patterns.each do | pattern |
+    start = pattern.split(" ")[2].tr(":", "").split(",")
+    x = start[0].to_i
+    y = start[1].to_i
+
+    sides = pattern.split(" ")[3].split("x")
+    lenx = sides[0].to_i
+    leny = sides[1].to_i
+
+    #puts lenx, leny
+
+    leny.times do |yy|
+        lenx.times do |xx|
+            fabric[y + yy][x + xx] += 1
+        end
+    end
+end
+
+fabric.each do |line|
+    line.each do |inch|
+        if inch > 1
+            overlaps += 1
+        end
+    end
+end
+
+puts overlaps
